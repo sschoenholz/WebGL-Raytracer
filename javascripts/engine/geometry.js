335c503cf0f3e14b53fcd6d83f6de5737a44892e
Samuel Schoenholz <sam.schoenholz@gmail.com>
2014-09-16 16:29:41 -0400

    A GPU ray tracer written in html5 and javascript.
    Here is a small ray tracer that I wrote in html5 and javascript using
    webGL. At the moment it supports spheres, planes, and point lights. No
    spatial optimization is done at the moment. Geometry is packed into
    textures and sent to the GPU.

---
+++ javascripts/engine/geometry.js
@@ -0,0 +1,195 @@
+function Raytracer(glCanvas) {
+	this.glCanvas = glCanvas;
+	this.objects = [];
+	this.lights = [];
+	
+	this.build = function() {
+		if(!(this.hasOwnProperty("vertexBuffer")))
+		{
+			this.vertexBuffer = this.glCanvas.createBuffer();
+			this.vertexBuffer.itemSize = 2;
+			this.vertexBuffer.numItems = 4;
+			
+			this.objectsTexture = this.glCanvas.createTexture();
+			this.objectDefinitionsTexture = this.glCanvas.createTexture();
+			this.objectMaterialsTexture = this.glCanvas.createTexture();
+			this.objectMaterialsExtendedTexture = this.glCanvas.createTexture();
+			
+			this.lightsTexture = this.glCanvas.createTexture();
+			this.lightMaterialsTexture = this.glCanvas.createTexture();
+		}	
+	
+		vertices = [
+			-1.0, 1.0,
+			-1.0, -1.0,
+			1.0, 1.0,
+			1.0, -1.0];
+			
+		this.glCanvas.bindBuffer(glCanvas.ARRAY_BUFFER,this.vertexBuffer);
+		this.glCanvas.bufferData(glCanvas.ARRAY_BUFFER,new Float32Array(vertices),glCanvas.STATIC_DRAW);
+	}
+	
+	this.buildScene = function () {
+		this.buildObjects();
+		this.buildDefinitions();
+	}
+	
+	this.buildObjects = function () {
+		//assemble the two objects
+		objectList = [];
+		objectPositions = [];
+		objectMaterials = [];
+		objectMaterialsExtended = [];
+		
+		for(i = 0 ; i < this.objects.length ; i++)
+		{
+			objectList = objectList.concat([i,this.objects[i].type,0,0]);
+			objectPositions = objectPositions.concat(this.objects[i].position);
+			objectMaterials = objectMaterials.concat(this.objects[i].material);
+			objectMaterialsExtended = objectMaterialsExtended.concat(this.objects[i].materialExtended);
+		}
+
+		//sizeList = Math.ceil(Math.sqrt(this.objects.length));
+		sizeList = Math.pow(2.0,Math.ceil(Math.log(this.objects.length)/(2.0*Math.log(2.0))));
+				
+		//fill the rest with zeros
+		for(i = 0 ; i < sizeList*sizeList - this.objects.length ; i++)
+		{
+			objectList = objectList.concat([0,0,0,0]);
+			objectPositions = objectPositions.concat([0.0,0.0,0.0,0.0]);
+			objectMaterials = objectMaterials.concat([0.0,0.0,0.0,0.0]);
+			objectMaterialsExtended = objectMaterialsExtended.concat([0.0,0.0,0.0,0.0]);
+		}
+				
+		dataList = new Uint8Array(objectList);
+		dataPositions = new Float32Array(objectPositions);
+		dataMaterials = new Float32Array(objectMaterials);
+		dataMaterialsExtended = new Float32Array(objectMaterialsExtended);
+    	    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectsTexture);
+    	glCanvas.texImage2D(glCanvas.TEXTURE_2D, 0, glCanvas.RGBA, sizeList, sizeList, 0, glCanvas.RGBA, glCanvas.UNSIGNED_BYTE, dataList);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MAG_FILTER, glCanvas.NEAREST);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MIN_FILTER, glCanvas.NEAREST);
+    	
+    	glCanvas.getExtension("OES_texture_float");
+    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectDefinitionsTexture);
+    	glCanvas.texImage2D(glCanvas.TEXTURE_2D, 0, glCanvas.RGBA, sizeList, sizeList, 0, glCanvas.RGBA, glCanvas.FLOAT, dataPositions);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MAG_FILTER, glCanvas.NEAREST);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MIN_FILTER, glCanvas.NEAREST);
+    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectMaterialsTexture);
+    	glCanvas.texImage2D(glCanvas.TEXTURE_2D, 0, glCanvas.RGBA, sizeList, sizeList, 0, glCanvas.RGBA, glCanvas.FLOAT, dataMaterials);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MAG_FILTER, glCanvas.NEAREST);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MIN_FILTER, glCanvas.NEAREST);
+    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectMaterialsExtendedTexture);
+    	glCanvas.texImage2D(glCanvas.TEXTURE_2D, 0, glCanvas.RGBA, sizeList, sizeList, 0, glCanvas.RGBA, glCanvas.FLOAT, dataMaterialsExtended);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MAG_FILTER, glCanvas.NEAREST);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MIN_FILTER, glCanvas.NEAREST);
+    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D,null);
+	}
+	
+	this.buildLights = function () {
+		//assemble the two objects
+		lightList = [];
+		lightMaterials = [];
+
+		for(i = 0 ; i < this.lights.length ; i++)
+		{
+			lightList = lightList.concat(this.lights[i].position);
+			lightMaterials = lightMaterials.concat(this.lights[i].material);
+		}
+		
+		sizeList = Math.pow(2.0,Math.ceil(Math.log(this.lights.length)/(2.0*Math.log(2.0))));
+		//sizeList = Math.ceil(Math.sqrt(this.lights.length));
+		
+		//fill the rest with zeros
+		for(i = 0 ; i < sizeList*sizeList - this.lights.length ; i++)
+		{
+			lightList = lightList.concat([0,0,0,0]);
+			lightMaterials = lightMaterials.concat([0.0,0.0,0.0,0.0]);
+		}
+				
+		dataPositions = new Float32Array(lightList);
+		dataMaterials = new Float32Array(lightMaterials);
+    	    	    	
+    	glCanvas.getExtension("OES_texture_float");
+    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.lightsTexture);
+    	glCanvas.texImage2D(glCanvas.TEXTURE_2D, 0, glCanvas.RGBA, sizeList, sizeList, 0, glCanvas.RGBA, glCanvas.FLOAT, dataPositions);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MAG_FILTER, glCanvas.NEAREST);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MIN_FILTER, glCanvas.NEAREST);
+    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.lightMaterialsTexture);
+    	glCanvas.texImage2D(glCanvas.TEXTURE_2D, 0, glCanvas.RGBA, sizeList, sizeList, 0, glCanvas.RGBA, glCanvas.FLOAT, dataMaterials);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MAG_FILTER, glCanvas.NEAREST);
+    	glCanvas.texParameteri(glCanvas.TEXTURE_2D, glCanvas.TEXTURE_MIN_FILTER, glCanvas.NEAREST);
+    	
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D,null);
+	}
+	
+	this.draw = function () {
+		glCanvas.bindBuffer(glCanvas.ARRAY_BUFFER,this.vertexBuffer);
+		glCanvas.vertexAttribPointer(glCanvas.shaderProgram.vertexPositionAttribute,this.vertexBuffer.itemSize,glCanvas.FLOAT,false,0,0);
+		
+		glCanvas.activeTexture(glCanvas.TEXTURE0);
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectsTexture);
+    	glCanvas.uniform1i(glCanvas.shaderProgram.objects, 0);
+    	
+    	glCanvas.activeTexture(glCanvas.TEXTURE1);
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectDefinitionsTexture);
+    	glCanvas.uniform1i(glCanvas.shaderProgram.objectDefinitions, 1);
+    	
+    	glCanvas.activeTexture(glCanvas.TEXTURE2);
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectMaterialsTexture);
+    	glCanvas.uniform1i(glCanvas.shaderProgram.objectMaterials, 2);
+    	
+    	glCanvas.activeTexture(glCanvas.TEXTURE3);
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.objectMaterialsExtendedTexture);
+    	glCanvas.uniform1i(glCanvas.shaderProgram.objectMaterialsExtended, 3);
+    	
+    	glCanvas.activeTexture(glCanvas.TEXTURE4);
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.lightsTexture);
+    	glCanvas.uniform1i(glCanvas.shaderProgram.lights, 4);
+    	
+    	glCanvas.activeTexture(glCanvas.TEXTURE5);
+    	glCanvas.bindTexture(glCanvas.TEXTURE_2D, this.lightMaterialsTexture);
+    	glCanvas.uniform1i(glCanvas.shaderProgram.lightMaterials, 5);
+    	
+    	glCanvas.uniform1i(glCanvas.shaderProgram.numObjects,this.objects.length);
+    	glCanvas.uniform1i(glCanvas.shaderProgram.numLights,this.lights.length);
+    	glCanvas.uniform1f(glCanvas.shaderProgram.objectTextureSize,Math.pow(2.0,Math.ceil(Math.log(this.objects.length)/(2.0*Math.log(2.0)))));
+    	glCanvas.uniform1f(glCanvas.shaderProgram.lightTextureSize,Math.pow(2.0,Math.ceil(Math.log(this.lights.length)/(2.0*Math.log(2.0)))));
+		
+		glCanvas.drawArrays(gl.TRIANGLE_STRIP, 0, this.vertexBuffer.numItems);
+	}
+	
+	return this;
+}
+
+function Sphere() {
+	this.type = 0;
+	this.position = [0.0,0.0,0.0,1.0];
+	this.material = [1.0,1.0,1.0,1.0];
+	this.materialExtended = [0.0,0.0,0.0,0.0];
+	
+	return this;
+}
+
+function Plane() {
+	this.type = 1;
+	this.position = [0.0,1.0,0.0,0.0];
+	this.material = [1.0,1.0,1.0,1.0];
+	this.materialExtended = [0.0,0.0,0.0,0.0];
+
+	return this;
+}
+
+function Light() {
+	this.position = [0.0,0.0,0.0,0.0];
+	this.material = [1.0,1.0,1.0,1.0];
+
+	return this;
+}
---
+++ javascripts/engine/opengl.js
@@ -0,0 +1,108 @@
+//Initialize an openGL context
+function InitializeWebGL(canvas) {
+  gl = null;
+  
+  try {
+    // Try to grab the standard context. If it fails, fallback to experimental.
+    gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+  }
+  catch(e) {}
+  
+  // If we don't have a GL context, give up now
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser may not support it.");
+    gl = null;
+  }
+  
+  if(gl){
+  	gl.viewportWidth = canvas.width;
+  	gl.viewportHeight = canvas.height;
+  }  
+  
+  InitializeShaders(gl);
+  
+  return gl;
+}
+
+//Initialize shaders
+function LoadShader(glCanvas,id)
+{
+	var shaderScript = document.getElementById(id);
+	if (!shaderScript) {
+		return null;
+	}
+
+	var str = "";
+	var k = shaderScript.firstChild;
+	while (k) {
+		if (k.nodeType == 3) {
+			str += k.textContent;
+		}
+		k = k.nextSibling;
+	}
+
+	var shader;
+	if (shaderScript.type == "x-shader/x-fragment") {
+		shader = glCanvas.createShader(glCanvas.FRAGMENT_SHADER);
+	} else if (shaderScript.type == "x-shader/x-vertex") {
+		shader = glCanvas.createShader(glCanvas.VERTEX_SHADER);
+	} else {
+		return null;
+	}
+
+	glCanvas.shaderSource(shader, str);
+	glCanvas.compileShader(shader);
+
+	if (!glCanvas.getShaderParameter(shader, glCanvas.COMPILE_STATUS)) {
+		alert(glCanvas.getShaderInfoLog(shader));
+		return null;
+	}
+
+	return shader;
+}
+
+function InitializeShaders(glCanvas)
+{
+	var fragmentShader = LoadShader(glCanvas,"shader-fs");
+	var vertexShader = LoadShader(glCanvas,"shader-vs");
+	
+	glCanvas.shaderProgram = glCanvas.createProgram();
+	
+	glCanvas.attachShader(glCanvas.shaderProgram,vertexShader);
+	glCanvas.attachShader(glCanvas.shaderProgram,fragmentShader);
+	glCanvas.linkProgram(glCanvas.shaderProgram);
+	
+	if(!glCanvas.getProgramParameter(glCanvas.shaderProgram,glCanvas.LINK_STATUS)) {
+		alert("Could not initialize shaders");
+	}
+	
+	glCanvas.useProgram(glCanvas.shaderProgram);
+	
+	glCanvas.shaderProgram.vertexPositionAttribute = glCanvas.getAttribLocation(glCanvas.shaderProgram,"aVertexPosition");
+	glCanvas.enableVertexAttribArray(glCanvas.shaderProgram.vertexPositionAttribute);
+			
+	glCanvas.shaderProgram.resolution = gl.getUniformLocation(glCanvas.shaderProgram,"uResolution");
+	glCanvas.uniform2f(glCanvas.shaderProgram.resolution,glCanvas.viewportWidth,glCanvas.viewportHeight);
+	
+	glCanvas.shaderProgram.objects = gl.getUniformLocation(glCanvas.shaderProgram,"objects");
+	glCanvas.shaderProgram.objectDefinitions = gl.getUniformLocation(glCanvas.shaderProgram,"objectDefinitions");
+	glCanvas.shaderProgram.objectMaterials = gl.getUniformLocation(glCanvas.shaderProgram,"objectMaterials");
+	glCanvas.shaderProgram.objectMaterialsExtended = gl.getUniformLocation(glCanvas.shaderProgram,"objectMaterialsExtended");
+	
+	glCanvas.shaderProgram.lights = gl.getUniformLocation(glCanvas.shaderProgram,"lights");
+	glCanvas.shaderProgram.lightMaterials = gl.getUniformLocation(glCanvas.shaderProgram,"lightMaterials");
+	
+	glCanvas.shaderProgram.numObjects = gl.getUniformLocation(glCanvas.shaderProgram,"numObjects");
+	glCanvas.shaderProgram.numLights = gl.getUniformLocation(glCanvas.shaderProgram,"numLights");
+	glCanvas.shaderProgram.objectTextureSize = gl.getUniformLocation(glCanvas.shaderProgram,"objectTextureSize");
+	glCanvas.shaderProgram.lightTextureSize = gl.getUniformLocation(glCanvas.shaderProgram,"lightTextureSize");
+		
+	InitializeResources(glCanvas);
+}
+
+function InitializeResources(glCanvas)
+{
+
+}
+
+
